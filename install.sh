c25e8d6e280ee325388ebd794419019a8566aa3c
Luigi Guevara <luigi.fox15@gmail.com>
2020-03-15 14:40:22 +0100

    Install and care of installation when files or directories exists

--- install.sh
+++ install.sh
@@ -49,6 +49,7 @@ done
 set -- "${POSITIONAL[@]}"
 
 LOCATION="${1:-$HOME}"
+CURRENT=$(pwd)
 
 cat <<EOF
 
@@ -63,3 +64,75 @@ We're about to install all dots in your ${LOCATION} directory
 If you want to know more, please ask for help: ``./install.sh -h|--help``
 
 EOF
+
+if [[ -d "${LOCATION}/.config/fish" ]]; then
+    if [[ -L "${LOCATION}/.config/fish" ]]; then
+        unlink "${LOCATION}/.config/fish"
+    else
+        mv "${LOCATION}/.config/fish" "${LOCATION}/.config/fish.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.config/fish" "${LOCATION}/.config/fish"
+
+echo "Fish installed ✅"
+
+if [[ -d "${LOCATION}/.config/karabiner" ]]; then
+    if [[ -L "${LOCATION}/.config/karabiner" ]]; then
+        unlink "${LOCATION}/.config/karabiner"
+    else
+        mv "${LOCATION}/.config/karabiner" "${LOCATION}/.config/karabiner.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.config/karabiner" "${LOCATION}/.config/karabiner"
+
+echo "Karabiner installed ✅"
+
+if [[ -f "${LOCATION}/.bash_profile" ]]; then
+    if [[ -L "${LOCATION}/.bash_profile" ]]; then
+        unlink "${LOCATION}/.bash_profile"
+    else
+        mv "${LOCATION}/.bash_profile" "${LOCATION}/.bash_profile.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.bash_profile" "${LOCATION}/.bash_profile"
+
+echo "Config bash_profile installed ✅"
+
+if [[ -f "${LOCATION}/.gitconfig" ]]; then
+    if [[ -L "${LOCATION}/.gitconfig" ]]; then
+        unlink "${LOCATION}/.gitconfig"
+    else
+        mv "${LOCATION}/.gitconfig" "${LOCATION}/.gitconfig.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.gitconfig" "${LOCATION}/.gitconfig"
+
+echo "Config gitconfig installed ✅"
+
+if [[ -f "${LOCATION}/.gitignore" ]]; then
+    if [[ -L "${LOCATION}/.gitignore" ]]; then
+        unlink "${LOCATION}/.gitignore"
+    else
+        mv "${LOCATION}/.gitignore" "${LOCATION}/.gitignore.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.gitignore.tmp" "${LOCATION}/.gitignore"
+
+echo "Config gitignore installed ✅"
+
+if [[ -f "${LOCATION}/.viminfo" ]]; then
+    if [[ -L "${LOCATION}/.viminfo" ]]; then
+        unlink "${LOCATION}/.viminfo"
+    else
+        mv "${LOCATION}/.viminfo" "${LOCATION}/.viminfo.bkp"
+    fi
+fi
+
+ln -s "${CURRENT}/.viminfo.tmp" "${LOCATION}/.viminfo"
+
+echo "Config viminfo installed ✅"
