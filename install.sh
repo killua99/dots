01b72736ffec29b5f35f8aa0116bf0feac2f5b07
Luigi Guevara <luigi.fox15@gmail.com>
2020-03-15 13:58:12 +0100

    Adding extra files and installation process

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+POSITIONAL=()
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case "$key" in
+        -h|--help)
+            cat <<EOF
+
+Comman usage:
+
+./install.sh [<path>] -d|--debug -h|--help
+
+Arguments:
+
+  🔰 path           Path where to install symlinks of dots
+
+Options:
+
+  🔰 d|--debug      Print commands
+  🔰 h|--help       Print this message
+
+Help:
+
+  This bash script helps to install dots in your $HOME or path you define:
+
+    ``./install.sh``
+
+    ``./install.sh /path/to/custom/home``
+
+EOF
+            exit 0
+            ;;
+        -d|--debug)
+            set -x
+            shift
+            ;;
+        *)
+            POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+PATH="${1:-HOME}"
+
+cat <<EOF
+
+We're about to install all dots in your ${PATH} directory
+
+    📌 .config/fish
+    📌 .config/karabiner
+    📌 .bash_profile
+    📌 .gitconfig
+    📌 .viminfo
+
+If you want to know more, please ask for help: ``./install.sh -h|--help``
+
+EOF
